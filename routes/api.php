55636e6633bad2dd039eb3bc9d9068a3a65e9ef1
kervisvasquez1993 <kervisvasquez24@gmail.com>
2021-03-11 19:56:23 -0400

    :sparkles: main add new routes type resource

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::resource('buyers', 'Buyer\BuyerController', ['only' => ['index', 'show']]);
+
+Route::resource('categories', 'Category\CategoryController', ['except' => ['create', 'edit']]);
+
+Route::resource('products', 'Product\ProductController', ['only' => ['index', 'show']]);
+
+Route::resource('transactions', 'Transaction\TransactionController', ['only' => ['index', 'show']]);
+
+Route::resource('sellers', 'Seller\SellerController', ['only' => ['index', 'show']]);
+
+Route::resource('users', 'User\UserController', ['except' => ['create', 'edit']]);
