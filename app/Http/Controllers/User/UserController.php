7f2493a7e6c0b99057c74a4889406bfb58162a99
kervisvasquez1993 <kervisvasquez24@gmail.com>
2021-03-14 17:20:19 -0400

    :sparkles: clase-14 add method generalizado para respuesta de la apirest

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -2,12 +2,12 @@
 
 namespace App\Http\Controllers\User;
 
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\ApiController;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
-class UserController extends Controller
+class UserController extends ApiController
 {
     /**
      * Display a listing of the resource.
